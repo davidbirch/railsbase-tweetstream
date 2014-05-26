672bf3916cfd6b013a15c69b94df6378726756ba
David Birch <birch.dave@gmail.com>
2014-05-26 11:14:58 +0000

    add raw tweet tests

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,10 @@
 class Tweet < ActiveRecord::Base
   belongs_to :twitter_user
+  
+  def tweet_text_as_stripped_array
+    tweet_text.split(' ').each do |t|
+      t.gsub!(/[,.]/,'')
+    end 
+  end
+  
 end
